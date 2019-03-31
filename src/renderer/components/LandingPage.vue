da42c360856a83bdc45cc611ee18fce770858a01
expecter <1005426244@qq.com>
2019-03-31 14:40:17 +0800

    111

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -2,7 +2,7 @@
 <div>
   <el-container>
     <el-header height = "120px">
-      哈哈11
+      哈哈111
     <el-input  v-model="input" placeholder="请输入内容"></el-input>
     <el-input  v-model="input" placeholder="请输入内容"></el-input>
     </el-header>
