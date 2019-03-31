a02a7990e5d7601cf98bf6f0dfceadb231ed102a
expecter <1005426244@qq.com>
2019-03-31 23:24:14 +0800

    111

--- src/renderer/components/pages/history.vue
+++ src/renderer/components/pages/history.vue
@@ -2,6 +2,15 @@
 <div>
     <el-table :data="rows"  border style="width: 100%" v-loading="pageLoading">
     <el-table-column label="1" prop = "0"></el-table-column>
+    <el-table-column label="1" prop = "1"></el-table-column>
+    <el-table-column label="1" prop = "2"></el-table-column>
+    <el-table-column label="1" prop = "3"></el-table-column>
+    <el-table-column label="1" prop = "4"></el-table-column>
+    <el-table-column label="1" prop = "5"></el-table-column>
+    <el-table-column label="1" prop = "6"></el-table-column>
+    <el-table-column label="1" prop = "7"></el-table-column>
+    <el-table-column label="1" prop = "8"></el-table-column>
+    <el-table-column label="1" prop = "9"></el-table-column>
     </el-table>
     <el-button type="primary" @click.native="handleSubmit" >提交</el-button>
 </div>
@@ -13,8 +22,6 @@ let handleSubmit = function () {
   this.pageLoading = true
   this.$http.get('https://api.api68.com/pks/getPksHistoryList.do?date=2019-03-21&lotCode=10001')
     .then((res) => {
-    // var priod = res.data.current.period
-    //   var tlData = res.data.result.data
     //   for (let i in tlData) {
     //     var data = tlData[i]
     //     var strs = data.preDrawCode.split(',')
@@ -26,7 +33,16 @@ let handleSubmit = function () {
     //       }
     //     }
     //   }
-      this.rows.push(['Baz11'])
+      this.rows = []
+      var tlData = res.data.result.data
+      for (let i in tlData) {
+        var strs = tlData[i].preDrawCode.split(',')
+        var rowData = {}
+        for (let j in strs) {
+          rowData[j] = strs[j]
+        }
+        this.rows.push(rowData)
+      }
       console.log(this.rows)
       this.pageLoading = false
     })
